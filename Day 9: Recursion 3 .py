10e8ea10b148b642f69e7efb765c9f5815ae616e
vigneshkanna <35601923+vigneshkanan@users.noreply.github.com>
2019-01-04 20:25:10 +0530

    Create Day 9: Recursion 3 .py

---
+++ Day 9: Recursion 3 .py
@@ -0,0 +1,14 @@
+import math
+import os
+import random
+import re
+import sys
+def factorial(num):
+    return 1 if num == 1 else factorial(num - 1) * num
+    print(factorial(int(input())))
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+     n = int(input())
+    result = factorial(n)
+    fptr.write(str(result) + '\n')
+    fptr.close()
